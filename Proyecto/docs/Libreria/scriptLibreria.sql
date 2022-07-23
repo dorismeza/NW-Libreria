b4bf603ee0bb4d1d951611470019f89159cd034b
dorismeza <dorismeza56@gmail.com>
2022-07-23 14:47:15 -0600

    Script de la base de datos

---
+++ Proyecto/docs/Libreria/scriptLibreria.sql
@@ -0,0 +1,350 @@
+CREATE DATABASE  IF NOT EXISTS `libreria` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `libreria`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: libreria
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autores`
+--
+
+DROP TABLE IF EXISTS `autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autores` (
+  `idAutor` int NOT NULL AUTO_INCREMENT,
+  `nombreAutor` varchar(45) NOT NULL,
+  `apellidoAutor` varchar(45) NOT NULL,
+  `genero` char(3) NOT NULL,
+  PRIMARY KEY (`idAutor`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autores`
+--
+
+LOCK TABLES `autores` WRITE;
+/*!40000 ALTER TABLE `autores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `autores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `carrito`
+--
+
+DROP TABLE IF EXISTS `carrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carrito` (
+  `idcarrito` int NOT NULL AUTO_INCREMENT,
+  `idlibro` varchar(64) NOT NULL,
+  `cantidad` int NOT NULL,
+  PRIMARY KEY (`idcarrito`),
+  KEY `libros_carrito_idx` (`idlibro`),
+  CONSTRAINT `libros_carrito` FOREIGN KEY (`idlibro`) REFERENCES `libros` (`idlibros`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carrito`
+--
+
+LOCK TABLES `carrito` WRITE;
+/*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `idCategorias` int NOT NULL AUTO_INCREMENT,
+  `categoriaDes` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCategorias`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funciones`
+--
+
+DROP TABLE IF EXISTS `funciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funciones` (
+  `fncod` varchar(255) NOT NULL,
+  `fndsc` varchar(45) DEFAULT NULL,
+  `fnest` char(3) DEFAULT NULL,
+  `fntyp` char(3) DEFAULT NULL,
+  PRIMARY KEY (`fncod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funciones`
+--
+
+LOCK TABLES `funciones` WRITE;
+/*!40000 ALTER TABLE `funciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funciones_roles`
+--
+
+DROP TABLE IF EXISTS `funciones_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funciones_roles` (
+  `rolescod` varchar(15) NOT NULL,
+  `fncod` varchar(255) NOT NULL,
+  `fnrolest` char(3) DEFAULT NULL,
+  `fnexp` datetime DEFAULT NULL,
+  PRIMARY KEY (`rolescod`,`fncod`),
+  KEY `rol_funcion_key_idx` (`fncod`),
+  CONSTRAINT `funcion_rol_key` FOREIGN KEY (`rolescod`) REFERENCES `roles` (`rolescod`),
+  CONSTRAINT `rol_funcion_key` FOREIGN KEY (`fncod`) REFERENCES `funciones` (`fncod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funciones_roles`
+--
+
+LOCK TABLES `funciones_roles` WRITE;
+/*!40000 ALTER TABLE `funciones_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funciones_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro_detalle`
+--
+
+DROP TABLE IF EXISTS `libro_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro_detalle` (
+  `idlibrodetalle` int NOT NULL AUTO_INCREMENT,
+  `idlibro` varchar(64) NOT NULL,
+  `stock` int NOT NULL,
+  `desc` int NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `descexp` datetime DEFAULT NULL,
+  `estaenoferta` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idlibrodetalle`),
+  KEY `detallelib_idx` (`idlibro`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro_detalle`
+--
+
+LOCK TABLES `libro_detalle` WRITE;
+/*!40000 ALTER TABLE `libro_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libro_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libros`
+--
+
+DROP TABLE IF EXISTS `libros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libros` (
+  `idlibros` varchar(64) NOT NULL,
+  `nombreLibro` varchar(45) NOT NULL,
+  `descripcion` text NOT NULL,
+  `coverart` text,
+  PRIMARY KEY (`idlibros`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Path de la portada del libro.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros`
+--
+
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libros_autores`
+--
+
+DROP TABLE IF EXISTS `libros_autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libros_autores` (
+  `idlibros` varchar(64) NOT NULL,
+  `idAutor` int NOT NULL,
+  PRIMARY KEY (`idlibros`,`idAutor`),
+  KEY `autores_idx` (`idAutor`),
+  KEY `libros_idx` (`idlibros`),
+  CONSTRAINT `autores` FOREIGN KEY (`idAutor`) REFERENCES `autores` (`idAutor`),
+  CONSTRAINT `libros_autores_fk` FOREIGN KEY (`idlibros`) REFERENCES `libros` (`idlibros`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros_autores`
+--
+
+LOCK TABLES `libros_autores` WRITE;
+/*!40000 ALTER TABLE `libros_autores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libros_autores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libros_categorias`
+--
+
+DROP TABLE IF EXISTS `libros_categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libros_categorias` (
+  `idCategoria` int NOT NULL,
+  `idlibros` varchar(64) NOT NULL,
+  PRIMARY KEY (`idCategoria`,`idlibros`),
+  KEY `calibros_idx` (`idlibros`),
+  CONSTRAINT `libros_categorias_categorias_idCategorias_fk` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategorias`),
+  CONSTRAINT `libros_categorias_fk` FOREIGN KEY (`idlibros`) REFERENCES `libros` (`idlibros`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros_categorias`
+--
+
+LOCK TABLES `libros_categorias` WRITE;
+/*!40000 ALTER TABLE `libros_categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libros_categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `rolescod` varchar(15) NOT NULL,
+  `rolesdsc` varchar(45) DEFAULT NULL,
+  `rolesest` char(3) DEFAULT NULL,
+  PRIMARY KEY (`rolescod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles_usuarios`
+--
+
+DROP TABLE IF EXISTS `roles_usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles_usuarios` (
+  `usercod` bigint NOT NULL,
+  `rolescod` varchar(15) NOT NULL,
+  `roleuserest` char(3) DEFAULT NULL,
+  `roleuserfch` datetime DEFAULT NULL,
+  `roleuserexp` datetime DEFAULT NULL,
+  PRIMARY KEY (`usercod`,`rolescod`),
+  KEY `rol_usuario_key_idx` (`rolescod`),
+  CONSTRAINT `rol_usuario_key` FOREIGN KEY (`rolescod`) REFERENCES `roles` (`rolescod`),
+  CONSTRAINT `usuario_rol_key` FOREIGN KEY (`usercod`) REFERENCES `usuario` (`usercod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles_usuarios`
+--
+
+LOCK TABLES `roles_usuarios` WRITE;
+/*!40000 ALTER TABLE `roles_usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles_usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `usercod` bigint NOT NULL AUTO_INCREMENT,
+  `useremail` varchar(80) DEFAULT NULL,
+  `username` varchar(80) DEFAULT NULL,
+  `userpswd` varchar(128) DEFAULT NULL,
+  `userfching` datetime DEFAULT NULL,
+  `userpswdest` char(3) DEFAULT NULL,
+  `userpswdexp` datetime DEFAULT NULL,
+  `userest` char(3) DEFAULT NULL,
+  `useractcod` varchar(128) DEFAULT NULL,
+  `userpswdchg` varchar(128) DEFAULT NULL,
+  `usertipo` char(3) DEFAULT NULL COMMENT 'Tipo de Usuario, Normal, Consultor o Cliente',
+  PRIMARY KEY (`usercod`),
+  UNIQUE KEY `useremail_UNIQUE` (`useremail`),
+  KEY `usertipo` (`usertipo`,`useremail`,`usercod`,`userest`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-23 14:38:28
